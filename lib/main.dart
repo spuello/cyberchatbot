1d1a664041d16498e6ef45f5c068bbd98f7bdc16
Samuel Puello <spuellocoronado@gmail.com>
2024-03-23 07:53:03 -0400

    Configure preferred orientation to be portrait up

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,16 @@
 import 'package:cyberchatbot/cyberbot_app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.light,
+    ),
+  );
   runApp(const CyberbotApp());
 }

97367ffb94bd24543befe4b9bff5b1fd9b8a7aa2
Samuel Puello <spuellocoronado@gmail.com>
2024-03-23 13:16:55 -0400

    Add scrollbar to ChatMessageInput

--- lib/Features/Chatroom /chat_message_input.dart
+++ lib/Features/Chatroom /chat_message_input.dart
@@ -1,14 +1,21 @@
-import 'package:cyberchatbot/common/dart_extensions/sizer_extension.dart';
-import 'package:flutter/material.dart';
 import 'dart:math' as math;
 
+import 'package:cyberchatbot/common/dart_extensions/sizer_extension.dart';
+import 'package:flutter/material.dart';
 
 import '../../theme/app_colors.dart';
 
-class ChatMessageInput extends StatelessWidget {
+class ChatMessageInput extends StatefulWidget {
   const ChatMessageInput({
     super.key,
   });
+
+  @override
+  State<ChatMessageInput> createState() => _ChatMessageInputState();
+}
+
+class _ChatMessageInputState extends State<ChatMessageInput> {
+  final _scrollController = ScrollController();
 
   @override
   Widget build(BuildContext context) {
@@ -20,52 +27,56 @@ class ChatMessageInput extends StatelessWidget {
           Expanded(
             child: ConstrainedBox(
               constraints: BoxConstraints(maxHeight: 150.dH),
-              child: TextField(
-                keyboardType: TextInputType.multiline,
-                maxLines: null,
-                decoration: InputDecoration(
-                  suffixIcon: IconButton(
-                    alignment: Alignment.bottomRight,
-                    padding: EdgeInsets.only(
-                        left: 8.dW, right: 16.dW, top: 12.dH, bottom: 12.dH),
-                    onPressed: () {},
-                    icon: Icon(
-                      Icons.photo_size_select_actual_rounded,
-                      color: AppColors.k8B8991,
-                      size: 32.dH,
+              child: Scrollbar(
+                controller: _scrollController,
+                child: TextField(
+                  scrollController: _scrollController,
+                  keyboardType: TextInputType.multiline,
+                  maxLines: null,
+                  decoration: InputDecoration(
+                    suffixIcon: IconButton(
+                      alignment: Alignment.bottomRight,
+                      padding: EdgeInsets.only(
+                          left: 8.dW, right: 16.dW, top: 12.dH, bottom: 12.dH),
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.photo_size_select_actual_rounded,
+                        color: AppColors.k8B8991,
+                        size: 32.dH,
+                      ),
                     ),
-                  ),
-                  hintText: "What's in you mind...",
-                  isDense: true,
-                  contentPadding: EdgeInsets.only(
-                      left: 16.dW, right: 24.dW, bottom: 12.dH, top: 12.dH),
-                  filled: true,
-                  fillColor: AppColors.kEEEEF3,
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: const BorderSide(
-                      color: Colors.transparent,
-                      width: 0.0,
-                      style: BorderStyle.none,
+                    hintText: "What's in you mind...",
+                    isDense: true,
+                    contentPadding: EdgeInsets.only(
+                        left: 16.dW, right: 24.dW, bottom: 12.dH, top: 12.dH),
+                    filled: true,
+                    fillColor: AppColors.kEEEEF3,
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(
+                        color: Colors.transparent,
+                        width: 0.0,
+                        style: BorderStyle.none,
+                      ),
+                      borderRadius: BorderRadius.circular(10.0),
                     ),
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: const BorderSide(
-                      color: Colors.transparent,
-                      width: 0.0,
-                      style: BorderStyle.none,
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(
+                        color: Colors.transparent,
+                        width: 0.0,
+                        style: BorderStyle.none,
+                      ),
+                      borderRadius: BorderRadius.circular(10.0),
                     ),
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  // errorBorder: InputBorder.none,
-                  // disabledBorder: InputBorder.none,
-                  border: OutlineInputBorder(
-                    borderSide: const BorderSide(
-                      color: Colors.transparent,
-                      width: 0.0,
-                      style: BorderStyle.none,
+                    // errorBorder: InputBorder.none,
+                    // disabledBorder: InputBorder.none,
+                    border: OutlineInputBorder(
+                      borderSide: const BorderSide(
+                        color: Colors.transparent,
+                        width: 0.0,
+                        style: BorderStyle.none,
+                      ),
+                      borderRadius: BorderRadius.circular(100),
                     ),
-                    borderRadius: BorderRadius.circular(100),
                   ),
                 ),
               ),
